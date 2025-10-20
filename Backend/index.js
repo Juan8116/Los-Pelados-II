f60dad23fb66d0e557cdadbd40e7ce0cdf13d8b7
Juan8116 <bg1373@est.ort.edu.ar>
2025-10-20 15:20:23 -0300

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,5 +11,14 @@ function enviarProductos() {
 function enviarPedido(producto, sbores, nombre) {
     let pedidos = JSON.parse(fs.readFileSync("data/pedidos.json", "utf-8"));
 
-    if (!Array.)
+    if (!Array.isArray(pedidos)) pedidos = [];
+    let Pedido = {producto: producto, sabores: sabores, nombre: nombre};
+    pedidos.push(Pedido);
+    let pedidosJSON = JSON.stringify(pedidos, null, 2)
+    fs.writeFileSync("data/pedidos.json", pedidoJSON);
+    return {ok: true};
 }
+subscribeGETEvent("sabores", enviarSabores);
+subscribeGETEvent("productos", enviarProductos);
+subscribePOSTEvent("pedido", enviarPedido);
+startServer();
