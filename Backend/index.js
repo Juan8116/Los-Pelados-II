221895ae152d3ec042a9dd03983b7686889de1b2
Juan8116 <bg1373@est.ort.edu.ar>
2025-12-01 09:52:54 -0300

    Termine Pelados 2

--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,27 @@
+import fs from "fs"
 import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
-import fs from "fs";
-function enviarSabores () {
-    const sabores = JSON.parse(fs.readFileSync("data/sabores.json", "utf-8"));
-    return sabores;
+let sabores = fs.readFileSync ("data/sabores.JSON", "utf-8")
+sabores = JSON.parse (sabores)
+let productos = fs.readFileSync ("data/productos.JSON", "utf-8")
+productos = JSON.parse (productos)
+let pedidos= fs.readFileSync("data/pedidos.JSON", "utf-8")
+pedidos= JSON.parse (pedidos)
+function leersabores (){
+sabores
+return sabores
 }
-function enviarProductos() {
-    const productos = JSON.parse(fs.readFyleSync("data/productos.json", "utf-8"));
-    return productos;
+function leerproductos () {
+productos
+return productos
 }
-function enviarPedido(producto, sabores, nombre) {
-    let pedidos = JSON.parse(fs.readFileSync("data/pedidos.json", "utf-8"));
-
-    if (!Array.isArray(pedidos)) pedidos = [];
-    let Pedido = {producto: producto, sabores: sabores, nombre: nombre};
-    pedidos.push(Pedido);
-    let pedidosJSON = JSON.stringify(pedidos, null, 2)
-    fs.writeFileSync("data/pedidos.json", pedidosJSON);
-    return {ok: true};
+function escribirpedidos (producto, sabores, nombre) {
+    let pedido = {cliente: nombre, producto: producto, sabores: sabores};
+    let pedidito = fs.readFileSync ("data/pedidos.json", "utf-8");
+    let pedidos = JSON.parse (pedidito)
+    pedidos.push (pedido)
+    fs.writeFileSync ("data/pedidos.json", JSON.stringify (pedidos,null,2));
 }
-subscribeGETEvent("sabores", enviarSabores);
-subscribeGETEvent("productos", enviarProductos);
-subscribePOSTEvent("pedido", enviarPedido);
-startServer();
+subscribeGETEvent ("sabores", leersabores)
+subscribeGETEvent ("productos", leerproductos)
+subscribePOSTEvent ("pedido", (producto, sabores, nombre) => escribirpedidos(producto, sabores, nombre))
+startServer (3000, true)
